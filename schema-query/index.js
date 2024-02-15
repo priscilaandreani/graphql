67646fbe5408f4930653c0df6ba842f88a2025db
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-02-15 18:32:33 -0300

    chore: add Apollo Server and GraphQL dependencies, and update package.json

---
+++ schema-query/index.js
@@ -0,0 +1,25 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { gql } from "graphql-tag";
+
+const typeDefs = gql`
+  # Entry point for the schema
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello, world!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server);
+
+console.log(`🚀 Server ready at ${url}`);
