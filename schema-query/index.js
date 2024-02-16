24ffad11b6cb88adf75882af13faf6affb996b4b
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-02-16 09:48:14 -0300

    feat: add Date scalar and hour field to Query

--- schema-query/index.js
+++ schema-query/index.js
@@ -3,15 +3,20 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { gql } from "graphql-tag";
 
 const typeDefs = gql`
+  # Custom scalar to represent a Date
+  scalar Date
+
   # Entry point for the schema
   type Query {
     hello: String
+    hour: Date
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => "Hello, world!",
+    hour: () => new Date(),
   },
 };
 
